be358f8a1f6fa753b829a212308b7c9a57cb05ab
Johannes Rackles <john@rackles.io>
2024-09-19 09:43:48 +0200

    chore: remove typography plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,5 +68,5 @@ export default {
     },
   },
   // eslint-disable-next-line @typescript-eslint/no-require-imports
-  plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
