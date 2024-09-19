5a84e2d31a01124081a15ecbe15bee0ea75f25ef
Johannes Rackles <john@rackles.io>
2024-09-19 09:45:18 +0200

    feat: make header logo bigger on large screens

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className="border-b border-muted">
       <div className="container mx-auto p-4 lg:p-8">
-        <Link to="/" className="font-semibold tracking-tight">
+        <Link to="/" className="font-semibold tracking-tight lg:text-2xl">
           H3VAL
         </Link>
       </div>
